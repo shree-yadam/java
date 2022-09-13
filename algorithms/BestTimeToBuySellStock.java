65a4cef48c1cb758be5b44aeba764d9ec8dbebcf
Shree Devi Yadam <shree.yadam@gmail.com>
2022-09-12 17:02:32 -0700

    Best time to buy and sell stock

---
+++ algorithms/BestTimeToBuySellStock.java
@@ -0,0 +1,18 @@
+class BestTimeToBuySellStock {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1) {
+            return 0;
+        }
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            if(prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
